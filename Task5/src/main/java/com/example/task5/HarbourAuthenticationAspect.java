b83a42a4edd6e87ba5e804a6284ff9c8a7776567
Neonchick <57732664+Neonchick@users.noreply.github.com>
2023-02-17 22:48:33 +0100

    add aspect

---
+++ Task5/src/main/java/com/example/task5/HarbourAuthenticationAspect.java
@@ -0,0 +1,9 @@
+package com.example.task5;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class HarbourAuthenticationAspect {
+}
